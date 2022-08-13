bad22f2f2f6c8f711989baa677532791d2aec6cc
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-08-13 18:17:19 +0900

    shellcheck1

--- src/scripts/collect_flaky_tests.sh
+++ src/scripts/collect_flaky_tests.sh
@@ -6,8 +6,8 @@ res=$(curl --request GET \
 --header "circle-token: $CIRCLE_TOKEN")
 
 # Skip if flaky tests don't exist
-flaky_tests_count=$(echo $res | jq '.total_flaky_tests')
-if [ $flaky_tests_count = '0' ]; then
+flaky_tests_count=$(echo "$res" | jq '.total_flaky_tests')
+if [ "$flaky_tests_count" = '0' ]; then
   echo "Flaky tests not found!"
   circleci-agent step halt
 fi
@@ -29,31 +29,31 @@ EOS
 )
 
 # Get each flaky tests & append to the template
-for i in $( seq 0 $(($flaky_tests_count - 1)) ); do
-  flaky_test=`echo ${res} | jq .flaky_tests[${i}]`
-  test_name=`echo ${flaky_test} | jq -r .test_name`
-  classname=`echo ${flaky_test} | jq -r .classname`
-  source=`echo ${flaky_test} | jq -r .source`
-  times_flaked=`echo ${flaky_test} | jq -r .times_flaked`
-  job_name=`echo ${flaky_test} | jq -r .job_name`
-  job_number=`echo ${flaky_test} | jq -r .job_number`
-  workflow_id=`echo ${flaky_test} | jq -r .workflow_id`
-  workflow_created_at=`echo ${flaky_test} | jq -r .workflow_created_at`
-  pipeline_number=`echo ${flaky_test} | jq -r .pipeline_number`
+for i in $( seq 0 $(("$flaky_tests_count" - 1)) ); do
+  flaky_test=$(echo ${res} | jq .flaky_tests[${i}])
+  test_name=$(echo ${flaky_test} | jq -r .test_name)
+  classname=$(echo ${flaky_test} | jq -r .classname)
+  source=$(echo ${flaky_test} | jq -r .source)
+  times_flaked=$(echo ${flaky_test} | jq -r .times_flaked)
+  job_name=$(echo ${flaky_test} | jq -r .job_name)
+  job_number=$(echo ${flaky_test} | jq -r .job_number)
+  workflow_id=$(echo ${flaky_test} | jq -r .workflow_id)
+  workflow_created_at=$(echo ${flaky_test} | jq -r .workflow_created_at)
+  pipeline_number=$(echo ${flaky_test} | jq -r .pipeline_number)
   
   # Create last flaked message using datediff
-  if [ `datediff ${workflow_created_at} today -f "%Y"` != '0' ]; then
-    last_flaked_message=$(datediff ${workflow_created_at} today -f "%Y years ago")
-  elif [ `datediff ${workflow_created_at} today -f "%m"` != '0' ]; then
-    last_flaked_message=$(datediff ${workflow_created_at} today -f "%m months ago")
-  elif [ `datediff ${workflow_created_at} today -f "%d"` != '0' ]; then
-    last_flaked_message=$(datediff ${workflow_created_at} today -f "%d days ago")
-  elif [ `datediff ${workflow_created_at} today -f "%H"` != '0' ]; then
-    last_flaked_message=$(datediff ${workflow_created_at} today -f "%H hours ago")
-  elif [ `datediff ${workflow_created_at} today -f "%M"` != '0' ]; then
-    last_flaked_message=$(datediff ${workflow_created_at} today -f "%M minutes ago")
+  if [ $(datediff "${workflow_created_at}" today -f "%Y") != '0' ]; then
+    last_flaked_message=$(datediff "${workflow_created_at}" today -f "%Y years ago")
+  elif [ $(datediff "${workflow_created_at}" today -f "%m") != '0' ]; then
+    last_flaked_message=$(datediff "${workflow_created_at}" today -f "%m months ago")
+  elif [ $(datediff "${workflow_created_at}" today -f "%d") != '0' ]; then
+    last_flaked_message=$(datediff "${workflow_created_at}" today -f "%d days ago")
+  elif [ $(datediff "${workflow_created_at}" today -f "%H") != '0' ]; then
+    last_flaked_message=$(datediff "${workflow_created_at}" today -f "%H hours ago")
+  elif [ $(datediff "${workflow_created_at}" today -f "%M") != '0' ]; then
+    last_flaked_message=$(datediff "${workflow_created_at}" today -f "%M minutes ago")
   else
-    last_flaked_message=$(datediff ${workflow_created_at} today -f "%S seconds ago")
+    last_flaked_message=$(datediff "${workflow_created_at}" today -f "%S seconds ago")
   fi
   
   flaky_test_template=$(cat << EOS
@@ -122,4 +122,4 @@ template=$(echo $template | jq ".blocks |= . + [$end_template]")
 
 # Add to environment vatiables
 echo $template | jq > /tmp/SlackTemplateForFlakyTests.json
-echo 'export SLACK_TEMPLATE_FOR_FLAKY_TESTS=$(cat /tmp/SlackTemplateForFlakyTests.json)' >> $BASH_ENV
+echo 'export SLACK_TEMPLATE_FOR_FLAKY_TESTS=$(cat /tmp/SlackTemplateForFlakyTests.json)' >> "$BASH_ENV"
