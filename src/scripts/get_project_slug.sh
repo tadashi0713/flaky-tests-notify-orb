81309ba6747cf2c3981fa493d3b226a92d937092
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-08-14 00:06:51 +0900

    Use test-insights

--- src/scripts/get_project_slug.sh
+++ src/scripts/get_project_slug.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -eu
 
-echo "export PROJECT_SLUG=$(echo "$CIRCLE_BUILD_URL" | sed -e "s|https://circleci.com/||g" -e "s|/[0-9]*$||g")" >> "$BASH_ENV"
+# echo "export PROJECT_SLUG=$(echo "$CIRCLE_BUILD_URL" | sed -e "s|https://circleci.com/||g" -e "s|/[0-9]*$||g")" >> "$BASH_ENV"
+
+echo "export PROJECT_SLUG='gh/tadashi0713/circleci-demo-test-insights'" >> "$BASH_ENV"
